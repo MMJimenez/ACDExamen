da5f24c0c66c0301cf28fc078fbd5130868e9e29
MMJimenez <munozmiguel321@gmail.com>
2022-12-12 12:16:25 +0100

    terminado Ejercicio 2

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,20 +11,22 @@ public class UsuarioDAO {
     private Connection connection;
     
     /* Completar consultas */
-    static final String INSERT_QUERY ="";
-    static final String LIST_QUERY="";
-    static final String GET_BY_DNI="";
-    
+    static final String INSERT_QUERY ="INSERT INTO examen.usuario (nombre, apellidos, dni) VALUES (?, ?, ?);";
+    static final String LIST_QUERY="SELECT * FROM examen.usuario;";
+    static final String GET_BY_DNI="SELECT * FROM examen.usuario WHERE dni = ?;";
+
+
+    private static final String USER = "newuser";
+    private static final String PASSWORD = "newuser";
+    private static final String URL = "jdbc:mysql://localhost:3306/examen";
     
     public void connect(){
         try {
-            
-            /* completar código de conexión */
-            
-            System.out.println("Base de datos no conectada");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Base de datos conectada");
         }catch(Exception ex){
             System.out.println("Error al conectar a la base de datos");
-            System.out.println("ex");
+            System.out.println(ex);
         }     
     }
     
@@ -38,7 +39,6 @@ public class UsuarioDAO {
     }
     
     public void save(Usuario user){
-        
         /**
          * Completar código para guardar un usuario 
          * Este método no debe generar el id del usuario, ya que la base
@@ -48,8 +48,22 @@ public class UsuarioDAO {
          * de forma que pase a tener el id correcto.
          */
 
-        System.out.println("Método save no completado");
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getNombre());
+            preparedStatement.setString(2, user.getApellidos());
+            preparedStatement.setString(3, user.getDni());
+            preparedStatement.executeUpdate();
 
+            ResultSet generatedId = preparedStatement.getGeneratedKeys();
+            if (generatedId.next()) {
+                user.setId(generatedId.getLong(1));
+            }
+
+            System.out.println("Introducido usuario correctamente con nuevo id: " + user.getId());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public ArrayList<Usuario> list(){
@@ -57,9 +71,24 @@ public class UsuarioDAO {
         var salida = new ArrayList<Usuario>(0);
         
         /* Completar código para devolver un arraylist con todos los usuarios */
-        
-        System.out.println("Método list no completado");
-        
+
+        try (var pst = connection.prepareStatement(LIST_QUERY)) {
+
+            ResultSet resultSet = pst.executeQuery();
+            while (resultSet.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId((long) resultSet.getInt("id"));
+                usuario.setNombre(resultSet.getString("nombre"));
+                usuario.setApellidos(resultSet.getString("apellidos"));
+                usuario.setDni(resultSet.getString("dni"));
+
+                salida.add(usuario);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
         return salida;
     }    
     
@@ -71,9 +100,26 @@ public class UsuarioDAO {
          * Completar código para devolver el usuario que tenga ese dni.
          * Si no existe, se debe devolver null
          */
-        
-        System.out.println("Método getByDNI no completado");
 
-        return salida;
+        try (var pst = connection.prepareStatement(GET_BY_DNI)) {
+
+            pst.setString(1, dni);
+
+            ResultSet resultSet = pst.executeQuery();
+
+            if (resultSet.next()) {
+                salida.setId((long) resultSet.getInt("id"));
+                salida.setNombre(resultSet.getString("nombre"));
+                salida.setApellidos(resultSet.getString("apellidos"));
+                salida.setDni(resultSet.getString("dni"));
+
+                return salida;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return null;
     }    
 }
